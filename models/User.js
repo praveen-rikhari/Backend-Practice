88458d151780e478114c7b3733bbec374ac77aa8
praveen-rikhari <praveenrikhari45@gmail.com>
2024-09-08 20:54:52 +0530

    updating user model

--- models/User.js
+++ models/User.js
@@ -4,19 +4,18 @@ const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: [true, 'Username is required'],
-        maxlength: [12, 'Username should not be greater than 12 characters.'],
     },
     email: {
         type: String,
         required: [true, 'Email is required'],
         unique: true,
-        match: [/.+\@.+\..+/, 'Please enter a valid email address'],
     },
     password: {
         type: String,
         required: [true, 'Password is required'],
-        minlength: [6, 'Password must be at least 6 characters long'],
     },
+}, {
+    timestamps: true,
 });
 
 module.exports = mongoose.model('User', userSchema);
