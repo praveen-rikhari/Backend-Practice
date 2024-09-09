22544c42be2b30673ff6b88cc091c90047c358a3
praveen-rikhari <praveenrikhari45@gmail.com>
2024-09-09 14:53:11 +0530

    adding user register logic

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const User = require('./models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.set('view engine', 'ejs');
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URL)
@@ -16,7 +24,49 @@ mongoose.connect(process.env.MONGO_URL)
     })
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the Movie Recommendation Platform');
+    res.render('index');
+});
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+app.post('/register', async (req, res) => {
+    const { username, password, email } = req.body;
+
+    try {
+        const existingUser = await User.findOne({ email });
+
+        if (existingUser) {
+            console.log("User already registered with this email");
+            return res.send("Already registered user");
+            // return res.json({ message: "Already registered user" });
+        }
+
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(password, salt, async (err, hash) => {
+                const newUser = await User.create({
+                    username,
+                    email,
+                    password: hash,
+                });
+
+                let token = jwt.sign({ email, userId: newUser._id }, process.env.JWT_SECRET);
+                res.cookie("token", token);
+                res.send("User registered sucessfully");
+            });
+        });
+
+        console.log("User registered successfully");
+        // return res.json({ message: "User Registered successfully" });
+    } catch (error) {
+        console.error(error);
+        // return res.json({ message: error });
+    }
+})
+
+app.get('/login', (req, res) => {
+    res.render('login');
 });
 
 const PORT = process.env.PORT || 3000;
