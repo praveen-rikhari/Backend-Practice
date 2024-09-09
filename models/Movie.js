30d0998cc9649c4f38a3a27ac7cd2b4b631126d3
praveen-rikhari <praveenrikhari45@gmail.com>
2024-09-09 21:40:38 +0530

    created movie model

---
+++ models/Movie.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const movieSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    imdbRating: {
+        type: Number,
+        required: true
+    },
+    genre: {
+        type: String,
+        required: true
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+});
+
+module.exports = mongoose.model('Movie', movieSchema);
+
